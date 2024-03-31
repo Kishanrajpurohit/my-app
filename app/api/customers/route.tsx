8cd68956adc863122e87e1d40d45017c4efff4fb
kishan singh <rajpurohitkishan01@gmail.com>
2024-03-31 13:10:05 +0530

    HELLO

--- app/api/customers/route.tsx
+++ app/api/customers/route.tsx
@@ -1,13 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import prisma from "@/prisma/client";
+const dayjs = require('dayjs')
+var postreqcalled = 0;
 
 
 export async function POST(request:NextRequest){
-
+  var date = dayjs().$d
+  var a = postreqcalled +1;
+postreqcalled = a;
+console.log(postreqcalled);
 const body = await request.json();
 const customer = await prisma.customers.create({
       data: {
+        
         customername : body.custname,
          mobilenumber : body.mobno,
          devicename : body.devicename,
